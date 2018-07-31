280135ad8a34cc016e710d3622871c0e977e9bc2
Duy Nguyen <dtn9797@gmail.com>
2018-07-31 09:54:12 -0500

    add more tests to androidTest

---
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/ApplicationTest.java
@@ -0,0 +1,15 @@
+package com.udacity.gradle.builditbigger;
+
+/**
+ * Created by duynguyen on 7/31/18.
+ */
+
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
